8b57f87ef2d678cb47ebc94d67980c04d89bdd17
nicko <37054216+nicko-lee@users.noreply.github.com>
2020-09-11 18:15:56 +1000

    Connect app to database First had to setup a new MongoDB Atlas cluster and add the credentials as an environment variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const articleRouter = require("../routes/articles");
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.set("view engine", "ejs");
 app.use("/articles", articleRouter);
