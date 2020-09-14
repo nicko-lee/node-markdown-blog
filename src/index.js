37d2deb320c3e6affb84e64475f7c6003a5f8324
nicko <37054216+nicko-lee@users.noreply.github.com>
2020-09-14 18:26:47 +1000

    Update homepage to render live data not mock This actually retrieves data from the database now and no longer just mocks article test data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Article = require("../models/article");
 const articleRouter = require("../routes/articles");
 const app = express();
 
@@ -12,20 +13,13 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use("/articles", articleRouter);
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test Article",
-      createdAt: new Date(),
-      description: "Test description"
-    },
-    {
-      title: "Test Article 2",
-      createdAt: new Date(),
-      description: "Test description"
-    }
-  ];
-  res.render("articles/index", { articles: articles, text: "apple" });
+app.get("/", async (req, res) => {
+  // get back an array of articles from Mongo
+  const articles = await Article.find().sort({ createdAt: "desc" }); // this gets everything in Article collection
+
+  res.render("articles/index", {
+    articles: articles
+  });
 });
 
 app.get("/foo", (req, res) => {
