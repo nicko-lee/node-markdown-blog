bab69708d8accb04b23214473d4c1a0c56200ea4
nicko <37054216+nicko-lee@users.noreply.github.com>
2020-09-16 18:51:49 +1000

    Implement edit functionality - build an edit page for the user - build the underlying PUT endpoint to actually update the article in the DB

--- routes/articles.js
+++ routes/articles.js
@@ -8,6 +8,12 @@ router.get("/new", (req, res) => {
 });
 
 // Page route
+router.get("/edit/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.render("articles/edit", { article: article });
+});
+
+// Page route
 router.get("/:slug", async (req, res) => {
   // query database search by id
   const article = await Article.findOne({ slug: req.params.slug }); // is an async func
@@ -18,26 +24,51 @@ router.get("/:slug", async (req, res) => {
 });
 
 // API route
-router.post("/", async (req, res) => {
-  // accessing user input and preparing an object to save to Mongo
-  let article = new Article({
-    title: req.body.title,
-    description: req.body.description,
-    markdown: req.body.markdown
-  });
+router.post(
+  "/",
+  async (req, res, next) => {
+    req.article = new Article(); // create an article and save it to our request
+    next(); // calls the saveArticle function below
+  },
+  saveArticleAndRedirect("new")
+);
 
-  try {
-    article = await article.save(); // this will return the article just saved to Mongo
-    res.redirect(`/articles/${article.slug}`);
-  } catch (e) {
-    res.render("articles/new", { articles: article });
-  }
-});
+router.put(
+  "/:id",
+  async (req, res, next) => {
+    req.article = await Article.findById(req.params.id);
+    next();
+  },
+  saveArticleAndRedirect("edit")
+);
 
 // API route
 router.delete("/:id", async (req, res) => {
   await Article.findByIdAndDelete(req.params.id);
   res.redirect("/");
 });
+
+// reusable function across POST and PUT (both are a form of saving)
+// this will return to us a set of middleware
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    // get article from our request instead of creating a new article - see previous commits
+    // and why do we have this because we did it in the step inside POST and PUT before calling next()
+    let article = req.article;
+    article.title = req.body.title;
+    article.description = req.body.description;
+    article.markdown = req.body.markdown;
+
+    try {
+      // save an article
+      article = await article.save(); // this will return the article just saved to Mongo
+      // redirect if successful
+      res.redirect(`/articles/${article.slug}`);
+    } catch (e) {
+      // if not successful just send back to the prior page
+      res.render(`articles/${path}`, { articles: article });
+    }
+  };
+}
 
 module.exports = router;
